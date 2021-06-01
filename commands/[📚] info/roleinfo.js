cbc46ff278dc3dcf19618f8cbbb11f5192bfa6a2
MiraBellierr <danialzamri0908@gmail.com>
2021-06-01 21:55:30 +0800

    Fix roleinfo not showing member count correctly

--- commands/[📚] info/roleinfo.js
+++ commands/[📚] info/roleinfo.js
@@ -8,28 +8,32 @@ module.exports = {
 	description: 'Returns role information',
 	example: `${bot_prefix}roleinfo <role name>`,
 	usage: '<role name>',
-	run: (client, message, args) => {
+	run: async (client, message, args) => {
 
 		const role = args.join(' ');
 		if(!role) return message.reply('Specify a role!');
 		const gRole = message.guild.roles.cache.get(role) || message.guild.roles.cache.find(r => r.name.toLowerCase().includes(role)) || message.mentions.roles.first();
 		if(!gRole) return message.reply('Couldn\'t find that role.');
 
-		const status = {
-			false: 'No',
-			true: 'Yes',
-		};
 
-		const roleemebed = new Discord.MessageEmbed()
-			.setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
-			.setTitle('Role Information')
-			.setDescription(`**• ID:** ${gRole.id}\n**• Name:** ${gRole.name}\n**• Mention:** ${gRole}\n**• Hex:** ${gRole.hexColor.toUpperCase()}\n**• Members with this role:** ${gRole.members.size}\n**• Position:** ${gRole.position}\n**• Hoisted status:** ${[gRole.hoist]}\n**• Mentionable:** ${status[gRole.mentionable]}\n**• Managed:** ${status[gRole.managed]}`)
-			.setColor('#00ff00')
-			.setThumbnail(message.guild.iconURL)
-			.setTimestamp()
-			.setFooter(client.user.tag, client.user.avatarURL({ dynamic: true }));
+		message.guild.members.fetch().then(members => {
+			const memberCount = members.filter(member => member.roles.cache.has(gRole.id)).size;
 
-		message.channel.send(roleemebed);
+			const status = {
+				false: 'No',
+				true: 'Yes',
+			};
 
+			const roleemebed = new Discord.MessageEmbed()
+				.setAuthor(message.author.username, message.author.displayAvatarURL({ dynamic: true }))
+				.setTitle('Role Information')
+				.setDescription(`**• ID:** ${gRole.id}\n**• Name:** ${gRole.name}\n**• Mention:** ${gRole}\n**• Hex:** ${gRole.hexColor.toUpperCase()}\n**• Members with this role:** ${memberCount}\n**• Position:** ${gRole.position}\n**• Hoisted status:** ${[gRole.hoist]}\n**• Mentionable:** ${status[gRole.mentionable]}\n**• Managed:** ${status[gRole.managed]}`)
+				.setColor('#00ff00')
+				.setThumbnail(message.guild.iconURL)
+				.setTimestamp()
+				.setFooter(client.user.tag, client.user.avatarURL({ dynamic: true }));
+
+			message.channel.send(roleemebed);
+		});
 	},
 };
