329bed4bd821edbacba975c4e18071002e3f68e5
Mira <56716823+MiraBellierr@users.noreply.github.com>
2021-06-06 21:40:40 +0800

    adjust ratelimit event

--- events/rateLimit.js
+++ events/rateLimit.js
@@ -1,3 +1,8 @@
 module.exports = (client, rateLimitInfo) => {
-	console.log(`[WARN] RATE LIMIT:\nrateLimitInfo: ${rateLimitInfo}`);
+	console.log('[WARN] RATE LIMIT:');
+	console.log(`[WARN] Timeout: ${rateLimitInfo.timeout}`);
+	console.log(`[WARN] Limit: ${rateLimitInfo.limit}`);
+	console.log(`[WARN] Method: ${rateLimitInfo.method}`);
+	console.log(`[WARN] Path: ${rateLimitInfo.path}`);
+	console.log(`[WARN] Route: ${rateLimitInfo.route}`);
 };
