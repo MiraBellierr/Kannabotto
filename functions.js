aed7d99d98165ace8c2485488acf64c9c6eb13ec
Mira <75848@siswa.unimas.my>
2021-08-26 12:05:31 +0800

    adjust the xpgain system

--- functions.js
+++ functions.js
@@ -1374,16 +1374,10 @@ module.exports = {
 		message.reply(`**${message.author.username}**, you have equipped ${emoji} ${weapon} to ${player.get('name')}!`);
 	},
 	xpGain: (level, enemyLevel) => {
-		let xpGain = Math.floor(100 / (level - enemyLevel));
+		let xpGain = Math.floor(Math.random() * (200 - 100) + 100);
 
-		if (level - enemyLevel > 100) {
-			xpGain = 1;
-		}
-		else if (xpGain === Infinity) {
-			xpGain = 150;
-		}
-		else if (xpGain < 1) {
-			xpGain = 100 + Math.floor(100 * (enemyLevel - level));
+		if (enemyLevel > level) {
+			xpGain += 100;
 		}
 
 		return xpGain;
