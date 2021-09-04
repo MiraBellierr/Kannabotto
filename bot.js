6a9797afdc512c2d49e166cf4d9a612e5955532d
Mira <75848@siswa.unimas.my>
2021-09-04 22:06:46 +0800

    add post stats

--- bot.js
+++ bot.js
@@ -13,9 +13,12 @@
 // limitations under the License.
 
 const { Client, Collection, MessageEmbed } = require('discord.js');
-const { TOKEN, bot_prefix, authtopggwebhook } = require('./config.json');
+const { TOKEN, bot_prefix } = require('./config.json');
 const fs = require('fs');
 const prefixes = require('./database/prefix.json');
+const topggVoteReward = require('./post/votereward/top.gg');
+const topgg = require('./post/top.gg');
+const discordbotlistcom = require('./post/discordbotlist.com');
 const Sequelize = require('sequelize');
 new Sequelize('database', 'user', 'password', {
 	host: 'localhost',
@@ -72,57 +75,12 @@ client.categories = fs.readdirSync('./commands/');
 	require(`./handlers/${handler}`)(client);
 });
 
-const topgg = require('./post/top.gg');
-
 setInterval(function() {
 	topgg(client);
+	discordbotlistcom(client);
 }, 1.8e+6);
 
-const Topgg = require('@top-gg/sdk');
-const express = require('express');
-const { createAllDataForNewUser, getUserDataAndCreate } = require('./functions');
-
-const app = express();
-
-const webhook = new Topgg.Webhook(authtopggwebhook);
-
-// vote reward
-app.post('/dblwebhook', webhook.listener(async vote => {
-	const Economy = Models.Economy();
-
-	await createAllDataForNewUser(vote.user);
-
-	const economy = await getUserDataAndCreate(Economy, vote.user);
-
-	let reward = 500;
-
-	const dayToday = new Date(Date.now()).getDay();
-
-	if (dayToday === 0 || dayToday === 6) {
-		reward = 1000;
-	}
-
-	const voteReward = economy.get('balance') + reward;
-
-	const channel = client.channels.cache.get('870151501950091264');
-
-	client.users.fetch(vote.user).then(async user => {
-		await Economy.update({ balance: voteReward }, { where: { userId: vote.user } });
-
-		const embed = new MessageEmbed()
-			.setTitle('Thank you for voting!')
-			.setColor('#ff1493')
-			.setThumbnail('https://cdn.discordapp.com/attachments/710732218254753842/845169239979589651/1383_bunny_holding_hearts.png')
-			.setDescription(`\`${user.tag} (${vote.user})\` just voted!\n${user.tag} received <a:jasminecoins:868105109748469780> ${reward === 1000 ? '1000 (doubled on weekend)' : '500'}\n\nYou can vote on top.gg [here](https://top.gg/bot/867048396358549544/vote) every 12 hours!`)
-			.setFooter('Thank you for your support!');
-		channel.send({ embeds: [embed] });
-		user.send(`Thank for voting! You just received <a:jasminecoins:868105109748469780> 500! ${reward === 1000 ? '\nBut hey, it\'s weekend, you received another <a:jasminecoins:868105109748469780> 500' : ''}`).catch((e) => console.log(e));
-	});
-
-
-}));
-
-app.listen(81);
+topggVoteReward(client);
 
 // boss spawner
 client.on('messageCreate', async message => {
