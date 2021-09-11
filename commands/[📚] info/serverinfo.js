27beb7995e60bb012d9008d0c368a9919dbea23e
Mira <75848@siswa.unimas.my>
2021-09-11 15:19:56 +0800

    fix serverinfo if there is no server features

--- commands/[📚] info/serverinfo.js
+++ commands/[📚] info/serverinfo.js
@@ -46,12 +46,15 @@ module.exports = {
 			.setTitle(guild.name)
 			.addField('Server Information 1', `**• Name:** ${guild.name}\n**• ID:** ${guild.id}\n**• Owner:** ${guild.members.cache.get(guild.ownerId)}\n**• Owner ID:** ${guild.ownerId}\n**• Created At:** ${created}\n**• Text Channels:** ${guild.channels.cache.filter(c => c.type === 'GUILD_TEXT').size} channels\n**• Voice Channels:** ${guild.channels.cache.filter(c => c.type === 'GUILD_VOICE').size} channels\n**• Roles:** ${guild.roles.cache.size} roles\n**• Emojis:** ${guild.emojis.cache.size} emojis\n**• Stickers:** ${guild.stickers.cache.size} stickers\n**• Humans:** ${guild.memberCount - guild.members.cache.filter(m => m.user.bot).size} humans\n**• Bots:** ${guild.members.cache.filter(m => m.user.bot).size} bots\n**• Total Members:** ${guild.memberCount} members\n**• Boost Count:** ${guild.premiumSubscriptionCount} boosts\n**• Shard:** ${guild.shard.id}`, true)
 			.addField('Server Information 2', `**• Name Acronym:** ${guild.nameAcronym}\n**• Icon URL:** [Link](${guild.iconURL({ dynamic: true, size: 4096 })})\n**• Large Server:** ${guild.large ? 'Yes' : 'No'}\n**• AFK Channel:** ${guild.afkChannel === null ? 'None' : guild.afkChannel}\n**• AFK Channel ID:** ${guild.afkChannelId === null ? 'None' : guild.afkChannelId}\n**• AFK Timeout:** ${guild.afkTimeout} Seconds\n**• Default Message Notifications:** ${guild.defaultMessageNotifications}\n**• Server Description:** ${guild.description === null ? 'None' : guild.description}\n**• Explicit Content Filter:** ${verlvl2[guild.explicitContentFilter]}\n**• Verification Level:** ${verlvl[guild.verificationLevel]}\n**• MFA Level:** ${guild.mfaLevel === 0 ? 'None' : 'High'}\n**• Partnered:** ${guild.partnered ? 'Yes' : 'No'}\n**• Verified:** ${guild.verified ? 'Yes' : 'No'}\n**• Vanity URL Code:** ${guild.vanityURLCode === null ? 'None' : guild.vanityURLCode}`, true)
-			.addField(`Server Features [${guild.premiumTier}]`, `${message.guild.features.map(f => `**• ${f}**`).join('\n')}`)
 			.setColor('0ED4DA')
 			.setThumbnail(guild.iconURL({ dynamic: true }))
 			.setImage(guild.bannerURL({ dynamic: true, size: 4096 }))
 			.setTimestamp()
 			.setFooter(client.user.tag, client.user.avatarURL({ dynamic: true }));
+
+		if (message.guild.features.size > 0) {
+			serverembed.addField(`Server Features [${guild.premiumTier}]`, `${message.guild.features.map(f => `**• ${f}**`).join('\n')}`);
+		}
 
 
 		message.reply({ embeds: [serverembed] });
