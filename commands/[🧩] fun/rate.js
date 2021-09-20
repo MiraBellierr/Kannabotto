96d0c34ce55f8b44ac130391d8076d8287953d43
Mira <75848@siswa.unimas.my>
2021-09-20 19:01:24 +0800

    fix emojis

--- commands/[🧩] fun/rate.js
+++ commands/[🧩] fun/rate.js
@@ -24,13 +24,13 @@ module.exports = {
 		const answers = [
 			// Postive
 			'💯', 'Yes', 'me likey', '👀', '😍😍', 'he\'s cool yea', 'she\'s cool yea', 'uhhhh yes', 'indeed', 'would bang', 'my favorite', 'pretty good', 'music to my ears',
-			'dreamy', 'Cool', 'at least it\'s not bad', 'not the best but still good', 'AMAZING', 'dude, that\'s like, awesome', `${client.emojis.cache.get('686924622637432902')}`,
-			`they're cute ${client.emojis.cache.get('662970076781346837')}`, 'underrated',
+			'dreamy', 'Cool', 'at least it\'s not bad', 'not the best but still good', 'AMAZING', 'dude, that\'s like, awesome', `${client.emojis.cache.get('872598470089912361')}`,
+			`they're cute ${client.emojis.cache.get('889465826254659654')}`, 'underrated',
 
 			// Negative
 			'how about no', 'yeah no', 'needs much improvement', 'barely ok, in short it\'s shit', '💩 basically', 'just horrible', 'never ask me to rate that again', 'overrated',
 			'nobody wants to see that', 'i disapprove', 'i\'m not allowed to say', 'that\'s goodn\'t', 'oh no', 'very uhh, how do i say this without sounding rude', 'might as well throw it away',
-			'this makes me wanna 🤮', 'what!! Lol', `you better be joking ${client.emojis.cache.get('703063200140230727')}`,
+			'this makes me wanna 🤮', 'what!! Lol', `you better be joking ${client.emojis.cache.get('889465826032377897')}`,
 		];
 
 		const answer = answers[Math.floor(Math.random() * answers.length)];
